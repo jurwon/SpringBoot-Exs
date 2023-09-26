c93744a41d2d57c07a042b2c840200aa49e18c10
jurwon <susorrow@naver.com>
2023-09-26 09:59:22 +0900

    0926 1교시 과제풀이 모듈 생성

---
+++ ch2-test-homework/ch2-test-homehork/src/main/java/com/example/ch2testhomehork/constant_sjw0926/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.ch2testhomehork.constant_sjw0926;
+
+public enum UserRole {
+    ADMIN,USER
+}
---
+++ ch2-test-homework/ch2-test-homehork/src/main/java/com/example/ch2testhomehork/entity_sjw0926/Member.java
@@ -0,0 +1,41 @@
+package com.example.ch2testhomehork.entity_sjw0926;
+
+import com.example.ch2testhomehork.constant_sjw0926.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;       //멤버코드
+
+
+    @Column(nullable = false, length = 50)
+    private String userName; //유저명
+
+    @Column(name="userEmail", nullable = false, unique = true)
+    private String userEmail; //이메일
+
+    @Lob
+    @Column(nullable = false)
+    private String userDescription; //유저 소개
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole; //유저 권한
+
+    private LocalDateTime regTime; //등록 시간
+
+
+}
