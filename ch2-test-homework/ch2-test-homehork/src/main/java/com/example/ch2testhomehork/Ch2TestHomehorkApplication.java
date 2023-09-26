9646112199162f2b864148e94810533f18415b0f
jurwon <susorrow@naver.com>
2023-09-26 10:00:24 +0900

    0926 1교시 과제풀이 모듈 생성

---
+++ ch2-test-homework/ch2-test-homehork/src/main/java/com/example/ch2testhomehork/Ch2TestHomehorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch2testhomehork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2TestHomehorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch2TestHomehorkApplication.class, args);
+    }
+
+}
---
+++ ch2-test-homework/ch2-test-homehork/src/test/java/com/example/ch2testhomehork/Ch2TestHomehorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch2testhomehork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2TestHomehorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
