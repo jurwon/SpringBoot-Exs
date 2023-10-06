d46b41657730a503cd00793d094ff00de875658c
jurwon <susorrow@naver.com>
2023-10-06 12:13:20 +0900

    1006 4교시 ch6-test 추가

---
+++ ch6-test/ch6-test/src/test/java/com/example/ch6test/Ch6TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch6test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch6TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
