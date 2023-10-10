35633c471237d2343074d4e07d9da3453dfa9382
jurwon <susorrow@naver.com>
2023-10-11 08:57:17 +0900

    1011 ch7~8test저장

---
+++ ch7_8test/ch7_8test/src/main/java/com/example/ch7_8test/Ch78testApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch7_8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch78testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch78testApplication.class, args);
+    }
+
+}
